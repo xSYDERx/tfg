b4915c8b7dece6e5765fafef30f4f572f3a5ac88
xSYDERx <ddpq.2804@gmail.com>
2024-06-15 00:03:34 +0200

    fixing style bug

--- gestor_reservas/pago.php
+++ gestor_reservas/pago.php
@@ -54,6 +54,7 @@ $conexion->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/header_y_footer.css">
+    <link rel="stylesheet" href="./css/login.css">
     <title>CBS Glup Glup</title>
     </head>
     <body>
--- gestor_reservas/reserva.php
+++ gestor_reservas/reserva.php
@@ -88,7 +88,7 @@ function generar_referencia_unica($id_servicio, $fecha_reserva) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/header_y_footer.css">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="./css/login.css">
     <title>CBS Glup Glup</title>
     </head>
 <header>
