73d53868c4b9f869e59ba8bd68e5086982523f37
xSYDERx <ddpq.2804@gmail.com>
2024-06-14 20:30:50 +0200

    reservas + actividades + bautizos done

--- gestor_reservas/reserva.php
+++ gestor_reservas/reserva.php
@@ -10,6 +10,14 @@ if (!isset($_SESSION['login'])) {
 // Obtener el login del usuario
 $login = $_SESSION['login'];
 
+// Obtener la actividad seleccionada
+$actividad = isset($_POST['actividad']) ? $_POST['actividad'] : '';
+
+if (empty($actividad)) {
+    echo "No se ha seleccionado ninguna actividad.";
+    exit();
+}
+
 // Verificar si se ha enviado el formulario de reserva
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['fecha_reserva'])) {
     // Conectar a la base de datos
@@ -85,9 +93,32 @@ function generar_referencia_unica($id_servicio, $fecha_reserva) {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Reserva</title>
-    <link rel="stylesheet" href="../css/estilo.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/header_y_footer.css">
+    <title>CBS Glup Glup</title>
 </head>
+<header>
+            <button id="menuButton"><img src="../assets/svg/blue_bg_menu.svg" alt=""></button>
+            <div>
+            <a href="../index.html" id="club_a">
+            <img src="../assets/img/logo_cbs.png" alt="Logo club" id="logo_club">
+            </a>
+            </div>
+            <nav id="mainMenu">
+            <ul>
+            <li><a href="../index.html">Inicio</a></li>
+            <li><a href="../html/who.html">El club</a></li>
+            <li><a href="../html/galeria.html">Galería</a>
+            </li>
+            <li><a href="../html/tienda.html">Tienda</a>
+            </li>
+            <li><a href="../html/contacto.html">Contacto</a></li>
+            <li><a href="../html/actividades.html">Realiza tu reserva</a></li>
+            <li id="user"><a href="">Inicio de sesión</a></li>
+            </ul>
+            </nav>
+            </header>
 <body>
     <h1>Reserva</h1>
 
@@ -137,7 +168,32 @@ function generar_referencia_unica($id_servicio, $fecha_reserva) {
     <input type="submit" name="reservar_sin_pago" value="Reservar sin realizar el pago">
 </form>
 <footer>
-    <p>Gestor de reservas BETA </p>
-    </footer>
+        <section id="fotos_footer">
+          <img src="" alt="">
+          <div id="logos">
+            <img src="../assets/img/instructor_trainer-removebg-preview.png" alt="logo acuc">
+            <img src="../assets/img/Cressi_pittogramma_neg.png" alt="logo cressi">
+          </div>
+        </section>
+          <section>
+            <h2 id="redes_title">Nuestras Redes sociales</h2>
+            <div id="redes">
+              <a href="https://www.youtube.com/@JoseGlup"><img src="../assets/svg/youtube-svgrepo-com.svg" alt="youtube"></a>
+              <a href="https://www.instagram.com/cbsglupglup/"><img src="../assets/svg/Instagram_logo_2016.svg" alt="instagram"></a>
+              <a href="https://www.facebook.com/cbglupglup"><img src="../assets/svg/Facebook_f_logo_(2019).svg" alt="facebook"></a>
+              <a href="https://www.tiktok.com/@cbsglupglup?lang=es"><img src="../assets/svg/tiktok-logo-logo-svgrepo-com.svg" alt="tiktok"></a>
+            </div>
+          </section>
+          <div id="legal">
+            <a href="../html/cookies.html">Política de cookies</a>
+            <a href="../html/legal.html">Aviso legal</a>
+            <a href="../html/privacidad.html">Política de privacidad</a>
+            </div>
+          <p>© 2024 CBS Glup Glup</p>
+      </footer>
+      <button id="scrollToTopBtn">&#8679;</button>
+
+      <script src="../js/gotopbutton.js"></script>
+      <script src="../js/burgermenu.js"></script>
 </body>
 </html>
